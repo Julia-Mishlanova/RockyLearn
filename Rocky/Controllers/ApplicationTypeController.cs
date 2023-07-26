26a6ee429570a2ad1174f16c91fa174e71788825
Julia-Mishlanova <mislanovaulia94@gmail.com>
2023-07-26 21:49:27 +0400

    Independent work on creating models, creating tables and adding a controller

---
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
+
+namespace Rocky.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _dB;
+
+        public ApplicationTypeController(ApplicationDbContext dB)
+        {
+            _dB = dB;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objList = _dB.ApplicationType;
+            return View(objList);
+        }
+    }
+}
