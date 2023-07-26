67e4710be795966e84f2d2fd0b5a46c7c14d421f
Julia-Mishlanova <mislanovaulia94@gmail.com>
2023-07-26 17:32:18 +0400

    Add project files.

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dB;
+
+        public CategoryController (ApplicationDbContext dB)
+        {
+            _dB = dB;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _dB.Category; 
+            return View(objList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
