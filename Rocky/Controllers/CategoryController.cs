3b664dc29b343db540a4148786561e5d86f52c35
Julia-Mishlanova <mislanovaulia94@gmail.com>
2023-07-27 02:23:26 +0400

    Add Edit and Delete functions

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -30,7 +30,73 @@ namespace Rocky.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _dB.Category.Add(obj);
+            if (ModelState.IsValid) 
+            {
+                _dB.Category.Add(obj);
+                _dB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET - Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _dB.Category.Find(id);
+
+            if (obj == null) 
+            { 
+                return NotFound(); 
+            }
+            return View(obj);
+        }
+
+        // POST - Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _dB.Category.Update(obj);
+                _dB.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET - Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _dB.Category.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+        {
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _dB.Category.Remove(obj);
             _dB.SaveChanges();
             return RedirectToAction("Index");
         }
