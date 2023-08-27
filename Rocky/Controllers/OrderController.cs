ab8023e4522818a767295a91d39a7d4a63b993c2
Julia-Mishlanova <mislanovaulia94@gmail.com>
2023-08-27 12:49:35 +0400

    Syncfusion formatting

---
+++ Rocky/Controllers/OrderController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Rocky.Interfaces;
+using Rocky.Utility.BrainTree;
+using Rocky_DataAccess;
+using Rocky_DataAccess.Repository.IRepository;
+using Rocky_Models;
+using Rocky_Models.ViewModels;
+using System.Linq;
+
+namespace Rocky.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHeaderRepos;
+        private readonly IOrderDetailRepository _orderDetailRepos;
+        private readonly IMailSender _mailSender;
+        private readonly IMailTemplater _mailTemplater;
+        private readonly IMessageFormater _messageFormater;
+        private readonly IBrainTreeGate _brain;
+
+        public ProductUserVM ProductUserVM { get; set; }
+        public OrderController(
+            IOrderHeaderRepository orderHeaderRepos,
+            IOrderDetailRepository orderDetailRepos,
+            IMailSender mailSender,
+            IMailTemplater mailTemplater,
+            IMessageFormater messageFormater,
+            IBrainTreeGate brain)
+        {
+            
+            _orderHeaderRepos = orderHeaderRepos;
+            _orderDetailRepos = orderDetailRepos;
+            _mailSender = mailSender;
+            _mailTemplater = mailTemplater;
+            _messageFormater = messageFormater;
+            _brain = brain;
+        }
+        public IActionResult Index()
+        {
+            OrderListVM orderListVM = new OrderListVM()
+            {
+                OrderHeaderList = _orderHeaderRepos.GetAll(),
+                StatusList = WC.ListStatus.ToList().Select(u => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem 
+                { 
+                    Text = u, Value = u
+                })
+            };
+            return View(orderListVM);
+        }
+    }
+}
--- Rocky_DataAccess/WC.cs
+++ Rocky_DataAccess/WC.cs
@@ -1,15 +1,19 @@
-﻿namespace Rocky_DataAccess
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Rocky_DataAccess
 {
     public static class WC
     {
         public const string ImagePath = @"\images\product\";
-        public const string SessionCart = @"ShoppingCartSession";
+        public const string SessionCart = "ShoppingCartSession";
         public const string SessionInquiryId = "InquirySession";
-
         public const string AdminRole = "Admin";
         public const string CustomerRole = "Customer";
 
-        public const string EmailAdmin = "qwerty123456789qwertyytrewq@gmail.com";
+        public const string EmailAdmin = "ben.spark90@yahoo.com";
+
 
         public const string CategoryName = "Category";
         public const string ApplicationTypeName = "ApplicationType";
@@ -23,5 +27,11 @@
         public const string StatusShipped = "Shipped";
         public const string StatusCancelled = "Cancelled";
         public const string StatusRefunded = "Refunded";
+
+        public static readonly IEnumerable<string> ListStatus = new ReadOnlyCollection<string>(
+            new List<string>
+            {
+                StatusApproved,StatusCancelled,StatusInProcess,StatusPending,StatusRefunded,StatusShipped
+            });
     }
 }
